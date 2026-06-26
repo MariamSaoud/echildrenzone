7ab267aee4f805a3290e75e46d3804aff800bc1a
MariamSaoud <lovelymarmoush.saoud14@gmail.com>
2026-06-27 00:00:25 +0300

    adding files validations

--- src/minio/minio.module.ts
+++ src/minio/minio.module.ts
@@ -1,11 +1,34 @@
-import { Module } from '@nestjs/common';
+import { BadRequestException, Module } from '@nestjs/common';
 import { MinioController } from './minio.controller';
 import { MinioProvider } from './minio.provider';
 import { MinioService } from './minio.service';
+import { MulterModule } from '@nestjs/platform-express';
 
 @Module({
   controllers: [MinioController],
   providers: [MinioProvider, MinioService],
   exports: [MinioProvider],
+  imports: [
+    MulterModule.register({
+      limits: {
+        fileSize: 25 * 1024 * 1024,
+      },
+      fileFilter: (req, file, callback) => {
+        const allowedMimeTypes = [
+          'image/jpeg',
+          'image/png',
+          'image/jpg',
+          'video/mp4',
+        ];
+        if (!allowedMimeTypes.includes(file.mimetype)) {
+          return callback(
+            new BadRequestException('Only image and videos files are allowed!'),
+            false,
+          );
+        }
+        callback(null, true);
+      },
+    }),
+  ],
 })
 export class MinioModule {}
