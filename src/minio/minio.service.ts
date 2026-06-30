cf32d73b8db5988edb344c6d9a5011f0c7f61b16
MariamSaoud <lovelymarmoush.saoud14@gmail.com>
2026-06-30 19:53:11 +0300

    add two buckets

--- src/minio/minio.service.ts
+++ src/minio/minio.service.ts
@@ -4,15 +4,31 @@ import * as Minio from 'minio';
 import { uuidv7 } from 'uuidv7';
 @Injectable()
 export class MinioService {
-  protected bucketName = 'echildrenzone';
+  protected photosBucketName = 'echildrenzonephoto';
+  protected videosBucketName = 'echildrenzonevideo';
   constructor(@InjectMinio() private readonly minioService: Minio.Client) {}
   async bucketList() {
     return await this.minioService.listBuckets();
   }
   async getFile(filename: string) {
+    const stat = await this.minioService.statObject(
+      'echildrenzonephoto',
+      filename,
+    );
+    const mimetype = stat.metaData['content-type'];
+    let bucketName;
+    if (
+      mimetype === 'image/jpeg' ||
+      mimetype === 'image/png' ||
+      mimetype === 'image/jpg'
+    ) {
+      bucketName = this.photosBucketName;
+    } else {
+      bucketName = this.videosBucketName;
+    }
     const data = await this.minioService.presignedUrl(
       'GET',
-      this.bucketName,
+      bucketName as string,
       filename,
       24 * 60 * 60,
       {
@@ -23,20 +39,20 @@ export class MinioService {
   }
   async uploadFile(file: Express.Multer.File) {
     try {
-      let folderName;
+      let bucketName;
       if (
         file.mimetype === 'image/jpeg' ||
         file.mimetype === 'image/png' ||
         file.mimetype === 'image/jpg'
       ) {
-        folderName = 'images';
+        bucketName = this.photosBucketName;
       } else {
-        folderName = 'videos';
+        bucketName = this.videosBucketName;
       }
-      const filename = `${folderName}/${uuidv7()}-${file.originalname}`;
+      const filename = `${uuidv7()}-${file.originalname}`;
 
       const objInfo = await this.minioService.putObject(
-        this.bucketName,
+        bucketName as string,
         filename,
 
         file.buffer,
